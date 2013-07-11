8964523712d6988951955c272238c5315f291572
YohannesTafesse <yz.tafesse@gmail.com>
2013-07-11 18:37:29 +0000

    3

--- web.js
+++ web.js
@@ -8,8 +8,10 @@ buf.toString()
 app.get('/', function(request, response) {
   
 var fs = require('fs');
-response.send(buf.toString('utc-8', fs.readFileSync("index.html")));
+fs.readFile('./index.html',function(err,data){
+  console.log(data.toString()); //replace console.log with the code to write to the response.
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
