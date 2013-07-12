e49e6d0d5066587f6ec34ef71a4efd1afae90bb7
YohannesTafesse <yz.tafesse@gmail.com>
2013-07-12 16:00:34 +0000

    21

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+
 
 var app = express.createServer(express.logger());
 
+var buf = new Buffer();
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+//response.send(fs.readFileSync('index.html'));
+response.send(buf.toString('utf8',0,fs.readFileSync('index.html')));
+  
 });
 
 var port = process.env.PORT || 5000;
