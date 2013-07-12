7c738f47d23178a0b949b7b6b37ad30d0f9f8683
YohannesTafesse <yz.tafesse@gmail.com>
2013-07-12 15:41:34 +0000

    Going back to first web.js

--- web.js
+++ web.js
@@ -2,29 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-
- 
-var fs = require('fs');
-/*fs.readFile('./index.html',function(err,data){
- console.log(data.toString());
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-}); */
-
-
-
-
-
-
-
-
-var buf = new Buffer();
-app.get('/', function(request, response) {
-    response.send(buf.toString('utf8',0,fs.readFileSync('index.html')));});
-
-var port = process.env.PORT || 5000;app.listen(port, function() {
-console.log("Listening on " + port);});
+});
