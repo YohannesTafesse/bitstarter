b6108bb3524808d50e3abb9c19e692fd6430160b
YohannesTafesse <yz.tafesse@gmail.com>
2013-07-12 14:39:43 +0000

    Another change on web.js

--- web.js
+++ web.js
@@ -5,12 +5,26 @@ var app = express.createServer(express.logger());
 
  
 var fs = require('fs');
-fs.readFile('./index.html',function(err,data){
-  console.log(data.toString());
+/*fs.readFile('./index.html',function(err,data){
+ console.log(data.toString());
 });
 
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+}); */
+
+
+
+
+
+
+
+
+var buf = new Buffer();
+app.get('/', function(request, response) {
+    response.send(buf.toString('utf8',0,fs.readFileSync('index.html')));});
+
+var port = process.env.PORT || 5000;app.listen(port, function() {
+console.log("Listening on " + port);});
