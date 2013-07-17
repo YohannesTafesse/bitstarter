f0e1a17fbaba2d0dfff8c1b166c6f563f74de99b
YohannesTafesse <yz.tafesse@gmail.com>
2013-07-17 20:16:03 +0000

    changed web.js for 8080

--- web.js
+++ web.js
@@ -12,7 +12,7 @@ response.send(buf.toString('utf8',0,fs.readFileSync('index.html')));
   
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
@@ -27,7 +27,7 @@ app.get('/', function(request, response) {
   response.send(readed);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
